1d4523ef87fb8e454499a05944c98bf5dc862e08
Sean Wu <seanwu1105@gmail.com>
2018-01-25 20:42:27 +0800

    complete the basic css file for lms website without icon replacement.

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+"use strict"
+// Check the webpage
 chrome.tabs.onUpdated.addListener(function (tagID, changeInfo, tab) {
-    if (new URL(tab.url).hostname == "lms.ncu.edu.tw") {
+    if (new URL(tab.url).hostname === "lms.ncu.edu.tw") {
         chrome.pageAction.show(tagID);
     }
 });
--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log("YO!")
+"use strict"
+
+// block the logout timer from counting down
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('CC.timer.clear();'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
+// add LMS logo
+var ccTabsL = document.getElementById('ccTabsL');
+var homepageLink = document.createElement('a');
+var logo = document.createElement('div');
+var icon = document.createElement('img');
+var title = document.createElement('span');
+homepageLink.href = ('https://lms.ncu.edu.tw/');
+logo.id = 'logo';
+icon.src = chrome.extension.getURL('images/NCULogo.svg');
+icon.id = 'logoIcon';
+title.id = 'logoTitle';
+title.innerHTML = 'LMS';
+logo.appendChild(icon);
+logo.appendChild(title)
+homepageLink.appendChild(logo);
+ccTabsL.appendChild(homepageLink);
+
+// change the #ccQTool DOM node
+var ccTabsR = document.getElementById('ccTabsR');
+ccTabsR.removeAttribute('nowrap')
+ccTabsR.appendChild(document.getElementById('ccQTool'));
+
+// change the .add DOM node
+var classAdd = document.getElementsByClassName('add');
+for (var i = 0; i < classAdd.length; i++) {
+    classAdd.item(i).appendChild(document.createTextNode('+'));
+}
