4deef2eb70d508e35784a253f915e7f451aa78a9
Sean Wu <seanwu1105@gmail.com>
2018-03-02 00:58:30 +0800

    change the z-index of #ccNavHeader for popup in LMS

--- scripts/document_start.js
+++ scripts/document_start.js
@@ -1,15 +1,8 @@
 "use strict";
 
-(function insertCSS() {
-    var lmsCSS = document.createElement('link');
-    lmsCSS.href = chrome.extension.getURL('main.css');
-    lmsCSS.id = 'insert';
-    lmsCSS.type = 'text/css';
-    lmsCSS.rel = 'stylesheet';
-    document.documentElement.appendChild(lmsCSS);
-})();
-
-function removeCSS() {
-    var lmsCSS = document.getElementById('insert');
-    lmsCSS.parentNode.removeChild(lmsCSS);
-}
+var lmsCSS = document.createElement('link');
+lmsCSS.href = chrome.extension.getURL('main.css');
+lmsCSS.id = 'insert';
+lmsCSS.type = 'text/css';
+lmsCSS.rel = 'stylesheet';
+document.documentElement.appendChild(lmsCSS);
