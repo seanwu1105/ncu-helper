1e66660f4e582d24edfa50230b16cbe8f8994e09
Sean Wu <seanwu1105@gmail.com>
2018-08-03 14:20:07 +0800

    add the communication between background.js and popup.js

--- scripts/background.js
+++ scripts/background.js
@@ -16,24 +16,72 @@ chrome.storage.sync.set({
 });
 
 // Get and set the alarm to update the info of NCU dorm netflow usage per minute
+let dormNetflowUsage = [];
 chrome.alarms.create('dormNetflowUsage', {periodInMinutes: 1});
-chrome.alarms.onAlarm.addListener((_alarm) => updateDormNetflowUsage(TEST_IP));
+chrome.alarms.onAlarm.addListener((_alarm) => updateDormNetflowUsage(
+    TEST_IP, dormNetflowUsage));
 
-updateDormNetflowUsage(TEST_IP);
+updateDormNetflowUsage(TEST_IP, dormNetflowUsage);
+
+// Deal with the request from popup.js
+chrome.runtime.onMessage.addListener(
+    (request, _sender, sendResponse) => {
+        if (request.name === 'dormNetflowUsage') {
+            sendResponse(dormNetflowUsage);
+        }
+    }
+);
+
+/**
+ * The netflow usage for a certain time.
+ */
+class NetflowUsage {
+    /**
+     * Construct a netflow usage data.
+     * @param {str} time A moment.js parsable string.
+     * @param {str|num} externalUpload The off compus upload netflow.
+     * @param {str|num} externalDownload The off compus download netflow.
+     * @param {str|num} totalUpload The total upload netflow.
+     * @param {str|num} totalDownload The total download netflow.
+     */
+    constructor(time, externalUpload, externalDownload,
+        totalUpload, totalDownload) {
+        this.time = time;
+        this.externalUpload = externalUpload;
+        this.externalDownload = externalDownload;
+        this.totalUpload = totalUpload;
+        this.totalDownload = totalDownload;
+    }
+
+    /**
+     * Get the internal upload netflow usage.
+     */
+    get internalUpload() {
+        return this.totalUpload - this.externalUpload;
+    }
+
+    /**
+     * Get the internal download netflow usage.
+     */
+    get internalDownload() {
+        return this.totalDownload - this.externalDownload;
+    }
+}
 
 /**
  * Send post request to `uncia.cc.ncu.edu.tw/dormnet` and get the 24 hrs dorm
- * net flow usage statistics.
- * @param {*} ipAddress The target ip address.
+ * net flow usage statistics. And save the result in netflowUsage.
+ * @param {str} ipAddress The target ip address.
+ * @param {[]} usageDataSet The return value of the callback of $.post,
+ *                          containing all netflow usage data in an array.
  */
-function updateDormNetflowUsage(ipAddress) {
+function updateDormNetflowUsage(ipAddress, usageDataSet) {
     let url = 'https://uncia.cc.ncu.edu.tw/dormnet/index.php';
     let proxy = 'https://cors-anywhere.herokuapp.com/';
     // Create a virtual document so that the browser does not automatically load
     // the images present in the supplied HTML.
     // More info: https://stackoverflow.com/questions/15113910/jquery-parse-html-without-loading-images/33825198#33825198
     let ownerDocument = document.implementation.createHTMLDocument('virtual');
-    let usageDataSet = [];
     $.post(proxy + url,
         {
             section: 'netflow',
@@ -43,26 +91,14 @@ function updateDormNetflowUsage(ipAddress) {
             console.log('updateDormNetflow status: ' + status);
             let table = $(html, ownerDocument).find(
                 'table[border="1"][cellspacing="0"][cellpadding="5"]');
-            $.each(
-                $(table).find('tr[bgcolor="#ffffee"], tr[bgcolor="#eeeeee"]'),
+            $.each($('tr[bgcolor="#ffffee"], tr[bgcolor="#eeeeee"]', table),
                 (_idx, tr) => {
-                    let data = {
-                        time: undefined,
-                        externalUl: undefined,
-                        externalDl: undefined,
-                        totalUl: undefined,
-                        totalDl: undefined,
-                    };
-                    $.each($(tr).find('td'), (idx, td) => {
-                        let content = $(td).text();
-                        if (idx === 0) data.time = moment(content);
-                        else if (idx === 1) data.externalUl = Number(content);
-                        else if (idx === 2) data.externalDl = Number(content);
-                        else if (idx === 3) data.totalUl = Number(content);
-                        else data.totalDl = Number(content);
-                    });
-                    usageDataSet.push(data);
+                    let data = [];
+                    $.each($('td', tr), (idx, td) => data[idx] = $(td).text());
+                    usageDataSet.push(new NetflowUsage(
+                        moment(data[0]), Number(data[1]), Number(data[2]),
+                        Number(data[3]), Number(data[4])
+                    ));
                 });
-            console.log(usageDataSet);
         });
 };
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,9 @@
 // Add settings.html href to link.
 document.getElementById('settings-link').setAttribute(
     'href', chrome.extension.getURL('settings.html'));
+
+// Get the dorm netflow info from background.js
+chrome.runtime.sendMessage(
+    {name: 'dormNetflowUsage'},
+    (response) => console.log(response)
+);
