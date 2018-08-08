7b0765d9e18cbb76d609fb8744e4bae00f38d37c
Sean Wu <seanwu1105@gmail.com>
2018-08-08 21:42:30 +0800

    finish the dorm netflow monitor!

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,25 @@
+const DORM_NETFLOW_LIMIT = 3 * 1024 * 1024 * 1024;
+
 // Add settings.html href to link.
 document.getElementById('settings-link').setAttribute(
     'href', chrome.extension.getURL('html/settings.html'));
 
 // create possible elements in #netflow-plot div
 let plot = document.getElementById('netflow-plot');
-let loading = document.createElement('div');
-loading.setAttribute(
-    'class', 'mdl-progress mdl-js-progress mdl-progress--indeterminate');
-let netflowIframe = document.createElement('iframe');
-netflowIframe.setAttribute('src', '../html/netflow.html');
-netflowIframe.setAttribute('class', 'netflow');
-let noDormNetflowSetInfo = document.createTextNode('尚未開啟宿網流量監控');
+let netflowPanel = plot.querySelector('div[style="display: none"]');
+let usageBar = plot.querySelector('div.mdl-progress');
+let usageDigit = plot.querySelector('p#netflow-usage-digit > span');
+let netflowIframe = plot.querySelector('iframe.netflow');
+let noDormNetflowSetInfo = plot.querySelector('p#no-info');
 
 /* Check whether the switch of dorm netflow monitoring is on. */
 
 chrome.storage.sync.get('dorm-netflow', (result) => {
-    if (result['dorm-netflow']) requestDormNetflowUsage();
-    else plot.appendChild(noDormNetflowSetInfo);
+    if (result['dorm-netflow']) {
+        netflowPanel.style.display = 'block';
+        componentHandler.upgradeDom();
+        requestDormNetflowUsage();
+    } else noDormNetflowSetInfo.style.display = 'block';
 });
 
 /**
@@ -28,24 +31,32 @@ function requestDormNetflowUsage() {
         (response) => {
             console.log('get response of dormNetflowUsage', response);
             if (response.length === 0) { // failed to get the netflow data
-                if (!plot.firstElementChild) {
-                    plot.appendChild(loading);
-                    componentHandler.upgradeDom();
-                }
                 setTimeout(requestDormNetflowUsage, 1000); // resend request
             } else {
-                $(loading).remove();
-                if (!plot.firstElementChild) plot.appendChild(netflowIframe);
+                netflowIframe.src = '../html/netflow.html';
+                usageBar.classList.remove('mdl-progress--indeterminate');
+                // Calculate the total usage (in Bytes).
+                let totalUsage = response.reduce((prev, cur) => {
+                    return prev + cur.externalUpload;
+                }, 0);
+                // Update the usage digit display.
+                usageDigit.innerHTML = Number.parseFloat(
+                    totalUsage / Math.pow(1024, 3)).toFixed(2) + '/3.00';
+                // Calculate the total usage ratio and update the progress bar.
+                usageBar.MaterialProgress.setProgress(
+                    Math.min(totalUsage / DORM_NETFLOW_LIMIT, 1) * 100
+                );
                 // Wait until the netflow iframe has fully loaded; then post
-                // the response as message.
-                netflowIframe.addEventListener('load', () =>
+                // the response as message to netflow.html (as iframe).
+                netflowIframe.addEventListener('load', () => {
                     netflowIframe.contentWindow.postMessage(
                         {
                             command: 'render',
                             context: response,
                         },
                         '*'
-                    ));
+                    );
+                });
             }
         }
     );
