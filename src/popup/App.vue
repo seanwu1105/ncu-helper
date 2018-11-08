ef55dc24619f8fa1e0853343850ac409928c6e2e
Sean Wu <seanwu1105@gmail.com>
2018-11-08 14:11:24 +0800

    update popup toolbar

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,20 +1,76 @@
 <template>
-  <div>
-    This is popup.
-  </div>
+  <v-app>
+    <v-toolbar app dense>
+      <img src="@/assets/logo.png" alt="NCU Helper" class="logo ma-1">
+      <v-toolbar-title>NCU Helper</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <v-btn slot="activator" icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+          v-for="(item, index) in menuItems"
+          :key="index"
+          @click="item.action"
+          target="_blank"
+          >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      menuItems: [
+        {
+          title: '計算 GPA',
+          action: () => { open('https://portal.ncu.edu.tw/system/162') },
+          icon: 'pages'
+        },
+        {
+          title: '設定',
+          action: () => { chrome.runtime.openOptionsPage() },
+          icon: 'settings'
+        },
+        {
+          title: '評分擴充程式',
+          action: () => { open('https://chrome.google.com/webstore/detail/ncu-helper/khhogbhcofdjjccjhgganhkhokibnfnb') },
+          icon: 'rate_review'
+        },
+        {
+          title: '錯誤回報',
+          action: () => { open('https://github.com/GLaDOS1105/ncu-helper/issues') },
+          icon: 'bug_report'
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
 html {
-    width: 400px;
-    height: 400px;
+  width: 400px;
+  min-height: 250px;
+}
+body {
+  font-size: initial;
+}
+.logo {
+  max-width: 70%;
+  max-height: 70%;
 }
 </style>
